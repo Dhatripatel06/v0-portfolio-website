e01f04b669d48f4d48d91018c96586d4c2201a55
dhatri patel <dhatripatel67@gmail.com>
2025-09-29 21:00:31 +0530

    Update contact.tsx and dependencies

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import type React from "react"
+import React from "react"
 
 import { useState } from "react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
